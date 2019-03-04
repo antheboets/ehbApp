651d32f189d535b554337559d10e4f3c9582b50e
Ilja <ilja.derycke@gmail.com>
2019-03-04 16:02:25 +0100

    Initial commit of new version

---
+++ TRAP/mobileproject/mobileproject/Views/MenuPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using mobileproject.Models;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobileproject.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        MainPage RootPage { get => Application.Current.MainPage as MainPage; }
+        List<HomeMenuItem> menuItems;
+        public MenuPage()
+        {
+            InitializeComponent();
+
+            //TODO: if user is manager/HR
+            if (true)
+            {
+                menuItems = new List<HomeMenuItem>
+                {
+                    new HomeMenuItem {Id = MenuItemType.Browse, Title="Timeline" },
+                    new HomeMenuItem {Id = MenuItemType.About, Title="Workweek" },
+                    new HomeMenuItem {Id = MenuItemType.About, Title="Projects" },
+                    new HomeMenuItem {Id = MenuItemType.About, Title="Activities" },
+                    new HomeMenuItem {Id = MenuItemType.About, Title="Settings" },
+                    new HomeMenuItem {Id = MenuItemType.About, Title="Logout" }
+                };
+            }
+            else
+            {
+                menuItems = new List<HomeMenuItem>
+                {
+                    new HomeMenuItem {Id = MenuItemType.Browse, Title="Timeline" },
+                    new HomeMenuItem {Id = MenuItemType.About, Title="Workweek" },
+                    new HomeMenuItem {Id = MenuItemType.About, Title="Settings" },
+                    new HomeMenuItem {Id = MenuItemType.About, Title="Logout" }
+                };
+            }
+
+
+            ListViewMenu.ItemsSource = menuItems;
+
+            ListViewMenu.SelectedItem = menuItems[0];
+            ListViewMenu.ItemSelected += async (sender, e) =>
+            {
+                if (e.SelectedItem == null)
+                    return;
+
+                var id = (int)((HomeMenuItem)e.SelectedItem).Id;
+                await RootPage.NavigateFromMenu(id);
+            };
+        }
+    }
+}
