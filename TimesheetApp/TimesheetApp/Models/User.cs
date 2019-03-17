149bd0f60c4c6967e27e90f82a221da2bfa2f2cd
antheboets <anthe.boets@gmail.com>
2019-03-17 23:06:25 +0100

    static logged in user

--- TimesheetApp/TimesheetApp/Models/User.cs
+++ TimesheetApp/TimesheetApp/Models/User.cs
@@ -6,6 +6,7 @@ namespace TimesheetApp.Models
 {
     class User
     {
+        public static User LoggedInUser { get; set; }
         public int Id { get; set; }
         public String Name { get; set; }
         public String Email { get; set; }
